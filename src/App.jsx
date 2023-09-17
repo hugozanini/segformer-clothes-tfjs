40ae6842ffc404df3b059ecc36e3c420fe327ebf
hugo zanini <hugozaninigomes@gmail.com>
2023-09-17 09:55:24 -0300

    removing loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
-import Loader from "./components/loader";
 import { Webcam } from "./utils/webcam";
 import { renderBoxes } from "./utils/renderBox";
 
